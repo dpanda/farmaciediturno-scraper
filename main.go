751f98c246ad75be61481389438bb856a1a7be5d
Daniele Panza <daniele.panza@satispay.com>
2018-08-16 17:57:57 +0200

    update response for API gateway, add some error handling

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"golang.org/x/net/context"
+	"encoding/json"
+	"log"
+	"os"
 )
+
+var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 
 type Pharmacy struct {
 	Name    string   `json:"name"`
@@ -29,21 +34,39 @@ func main() {
 	*/
 }
 
-func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) ([]Pharmacy, error) {
-	pharmas := scrape(request.QueryStringParameters["address"])
-	return pharmas, nil //events.APIGatewayProxyResponse{Body: pharmas, Headers: headers, StatusCode: 200}, nil
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	pharmas, err := scrape(request.QueryStringParameters["address"])
+	if err != nil {
+		return serverError(err)
+	}
+
+	jsonPharmas, err := json.Marshal(pharmas)
+	if err != nil {
+		return serverError(err)
+	}
+	//return pharmas, nil //events.APIGatewayProxyResponse{Body: pharmas, Headers: headers, StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(jsonPharmas),
+	}, nil
 }
 
-func scrape(address string) ([]Pharmacy) {
+func scrape(address string) ([]Pharmacy, error) {
 
-	resp, _ := http.Get("https://www.farmaciediturno.org/ricercaditurno.asp?indirizzo=" + url.QueryEscape(address))
-	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+	resp, err := http.Get("https://www.farmaciediturno.org/ricercaditurno.asp?indirizzo=" + url.QueryEscape(address))
+	if err != nil {
+		return nil, err
+	}
+	htmlDoc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	phoneregexp, _ := regexp.Compile("[0-9]{8,15}")
 
 	pharmas := []Pharmacy{}
 
-	doc.Find(".sf.mnu.c").NextAll().Each(func(i int, s *goquery.Selection) {
+	htmlDoc.Find(".sf.mnu.c").NextAll().Each(func(i int, s *goquery.Selection) {
 
 		btag := s.Find("td.bbo b")
 
@@ -65,5 +88,21 @@ func scrape(address string) ([]Pharmacy) {
 
 	})
 
-	return pharmas
+	return pharmas, nil
+}
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	errorLogger.Println(err.Error())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       http.StatusText(http.StatusInternalServerError),
+	}, nil
+}
+
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}, nil
 }
